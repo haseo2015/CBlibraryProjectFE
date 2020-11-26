eb74ee7b15583a2badc4c74d959c1a71a48a3371
forkuser <forkuser@mailinator.com>
2020-11-26 22:10:36 +0100

    Shared component revisited

--- shared/index.js
+++ shared/index.js
@@ -5,27 +5,18 @@ const logDate = new Date().toISOString();
 const logName = 'debug.log';
 let log_file
 
-const logger = {
-  initLog: () => {
-    log_file = fs.createWriteStream(__dirname + `/${logName}`, {flags : 'w'});
-  },
-  writeLog: info => {    
-    try{
-      if (fs.existsSync(__dirname + `/${logName}`)) {
-        log_file.write(util.format(`${logDate} - ${info}`) + '\n');
-      } else {
-        logger.initLog()
-        log_file.write(util.format(`${logDate} - ${info}`) + '\n');
-      }
-      
-    } catch(err) {
-      console.error(err)
-    }
+exports.initLog = initLog;
+exports.writeLog = writeLog;
+
+function initLog () {
+  log_file = fs.createWriteStream(__dirname + `/${logName}`, {flags : 'w'});
+};
+
+function writeLog (info) { 
+  if (fs.existsSync(__dirname + `/${logName}`)) {
+    log_file.write(util.format(`${logDate} - ${info}`) + '\n');
+  } else {
+    logger.initLog()
+    log_file.write(util.format(`${logDate} - ${info}`) + '\n');
   }
-
 }
-
-
-
-
-module.exports = logger
